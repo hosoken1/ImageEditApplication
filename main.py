26b6544841e404bd58bddaf4617e7b57c19a4264
hosoken1 <hosoken1edm@gmail.com>
2022-10-06 20:51:28 +0900

    変数名などのスペルミスの修正

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def ExitApplication():
     sys.exit()
 
 #ファイルの閲覧
-def OpenFileOnExploror():
+def OpenFileOnExplorer():
     inipath = '/'
     filepath = filedialog.askopenfilename()
     entry.delete(0,tk.END)
@@ -44,14 +44,14 @@ def FileImageExist():
 text = ttk.Label(frame, text='フォルダ指定：')
 text_error = ttk.Label(frame, text='',foreground='#ff0000')
 entry = ttk.Entry(frame)
-button_fopen = ttk.Button(frame, text='参照', command=lambda:OpenFileOnExploror())
+button_FileOpen = ttk.Button(frame, text='参照', command=lambda:OpenFileOnExplorer())
 button_execute = ttk.Button(frame, text='実行', command=lambda:FileImageExist())
 button_exit = ttk.Button(frame,text='終了',command = lambda:ExitApplication())
 
 # 各種ウィジェットの設置
 text.grid(row=0, column=0)
 entry.grid(row=0, column=1,ipadx=40)
-button_fopen.grid(row=0, column=2)
+button_FileOpen.grid(row=0, column=2)
 button_execute.grid(row=1, column=1)
 text_error.grid(row=2,column=1)
 button_exit.grid(row=3,column=0)
